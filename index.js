9fd2a20f1ee36686815459f08342d2ad10cff8d9
nikhil <nikhiltelase@gmail.com>
2024-08-13 23:46:43 +0530

    add

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://my-shopee-beryl.vercel.app/'
 }));
 
 
