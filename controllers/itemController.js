d1240d80bf162a56342e719e956aa57fba3851cb
nikhil <nikhiltelase@gmail.com>
2024-09-15 21:12:40 +0530

    add infinite scroll

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,8 +3,12 @@ import Item from "../models/itemModel.js";
 //get all items
 export const allItems = async (req, res) => {
   try {
+    const { limit, start } = req.query;
+
     const items = await Item.find();
-    return res.status(200).json({ success: true, items });
+    const sendItems = items.slice(start, Number(start) + Number(limit));
+
+    return res.status(200).json({ success: true, items: sendItems });
   } catch (error) {
     return res.status(404).json({ success: false, message: error.message });
   }
